f9011ca7df3ac6d0261313dc710245a24f797ea7
Rémi <craco38play@gmail.com>
2021-11-30 16:25:55 +0100

    haut de page

--- resources/cache/1298774c5da32ea38f460f6fb10d547a36eeb634.php
+++ resources/cache/1298774c5da32ea38f460f6fb10d547a36eeb634.php
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<?php
-$titre="Profile";
-?>
-
-
-<?php $__env->startSection('content'); ?>
-    <p>Et voici la page 2</p>
-<?php $__env->stopSection(); ?>
-=======
 <?php
 $titre="Profile";
 ?>
@@ -19,5 +9,4 @@ $titre="Profile";
 <?php $__env->stopSection(); ?>
 
 
->>>>>>> 5f9c9e6ead025d60db5f4acfb40425ee3cdd4c5d
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,20 @@
-<link type="text/css" rel="stylesheet" href="../../assets/style.css">
-
 @isset($_SESSION['info'])
     <div>
         <strong>Information : </strong> {{$_SESSION['info']}}
     </div>
 @endisset
 
-<nav>
-    <a href="index.php?action=index">KindBook</a>
-    @include('partials.search')
-    <a href="index.php?action=filsacc">Fils D'acctualité</a>
-
-    @isset($_SESSION['id'])
-    <a href='index.php?action=profile'>{{$_SESSION['login']}} </a> </li>
-    @else
-        <a href='index.php?action=login'>Login</a>
-    @endif
-</nav>
+<header>
+    <nav class="haut-nav">
+        <div class="haut-logo_recherche">
+            <li class="haut-logo"><h1><a href="index.php?action=index"><span class="logo_kinbook">K<span class="logo_kinbook_rouge">indBook</span></span></a></h1></li>
+            <li>@include('partials.search')</li>
+        </div>
+        <li class="haut-fil_actu"><a href="index.php?action=filsacc"><img src="img\livre.png" alt="livre"><span>Fil d'actualité</span> </a></li>
+        <li class="haut-nom-utilisateur"><a href='index.php?action=profile'><img src="img\utilisateur.png" alt="utilisateur"><span>Nom d'utilisateur <!--{{$_SESSION['login']}}--> </span> </a></li>
+        <div class="haut-statut_deco">
+            <li class="haut-statut"><a href="index.php?action=statut"> Statut : En ligne <img src="img\en_ligne.png" alt="En ligne"></a></li>
+            <li class="haut-deco"><a href="index.php?action=deconnexion">Déconnexion <img src="img\deconnexion.png" alt="déconnexion"></a> </li>
+        </div>
+    </nav>
+</header>
